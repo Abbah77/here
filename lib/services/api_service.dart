919a2e27807aacef4d27f9eebcd7af7e04f22ada
Abbah77 <yakubuyakson77@gmail.com>
2026-03-09 17:15:16 +0100

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -9,7 +9,7 @@ class ApiService {
   ApiService._internal();
 
   // Change this URL when your backend changes
-  static const String baseUrl = 'http://10.65.226.255:8000/api';
+  static const String baseUrl = 'http://localhost:8000/api';
   
   // Headers
   static const Map<String, String> headers = {
